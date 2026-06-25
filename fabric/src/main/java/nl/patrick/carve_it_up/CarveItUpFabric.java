193cd307cbe590f2fd2fdceb35e85ecb21ec726d
McPatrickNL <sloot.patrick@gmail.com>
2026-06-25 11:25:10 +0200

    Working Fabric instance.

--- fabric/src/main/java/nl/patrick/carve_it_up/CarveItUpFabric.java
+++ fabric/src/main/java/nl/patrick/carve_it_up/CarveItUpFabric.java
@@ -1,22 +1,38 @@
 package nl.patrick.carve_it_up;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
 import nl.patrick.carve_it_up.block.ModBlocks;
 import nl.patrick.carve_it_up.item.ModItems;
-import nl.patrick.carve_it_up.tab.ModCreativeModeTabs;
 
 // File Location from project root:
 // fabric/src/main/java/nl/patrick/carve_it_up/CarveItUpFabric.java
 
 public class CarveItUpFabric implements ModInitializer
 {
+    // todo in fabric.mod.json:
+    //  I removed "icon": "assets/carve_it_up/icon.png", after the license line.
+    //  I removed (before "depends"):
+    //  "mixins": [
+    //    "carve_it_up.mixins.json"
+    //  ],
+    
     @Override
     public void onInitialize() {
         // 1. Trigger the common registration block (Registers blocks/items/tabs)
         CarveItUpCommon.init();
         
+        ResourceKey<CreativeModeTab> tabKey = ResourceKey.create(
+                Registries.CREATIVE_MODE_TAB,
+                Identifier.fromNamespaceAndPath(CarveItUpCommon.MOD_ID, "carve_it_up_tab")
+        );
+        
         // 2. Populate your multiplatform creative tab on Fabric
-        ItemGroupEvents.modifyEntriesEvent(ModCreativeModeTabs.CARVE_IT_UP_TAB.getKey()).register(content -> {
+        CreativeModeTabEvents.modifyOutputEvent(tabKey).register(content -> {
             content.accept(ModItems.EXAMPLE_ITEM.get());
             content.accept(ModBlocks.EXAMPLE_BLOCK_ITEM.get());
         });
