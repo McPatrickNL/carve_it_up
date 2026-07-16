8ccfe37d3be0adbb1eeb2777c1479c353085c919
McPatrickNL <sloot.patrick@gmail.com>
2026-07-16 15:47:17 +0200

    Fixed Fabric gradle. It now properly accesses Common resources.

--- common/src/main/java/nl/patrick/carve_it_up/carving/CarvingKeyBinds.java
+++ common/src/main/java/nl/patrick/carve_it_up/carving/CarvingKeyBinds.java
@@ -12,21 +12,21 @@ import org.lwjgl.glfw.GLFW;
 
 public class CarvingKeyBinds
 {
-    public static final KeyMapping.Category CIU_CARVING_MENU = KeyMapping.Category.register(
-            Identifier.fromNamespaceAndPath(CommonMod.MOD_ID, "key.categories.carve_it_up"));
+    public static final KeyMapping.Category CIU_CATEGORY = KeyMapping.Category.register(
+            Identifier.fromNamespaceAndPath(CommonMod.MOD_ID, "carve_it_up"));
     
     // Registered KeyMappings allowing customization in Minecraft's Controls menu
     public static final KeyMapping CATEGORY_KEY = new KeyMapping(
             "carve_it_up.key.category_key",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_LEFT_CONTROL,
-            CIU_CARVING_MENU
+            CIU_CATEGORY
     );
     
     public static final KeyMapping SUBMENU_KEY = new KeyMapping(
             "carve_it_up.key.submenu_key",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_LEFT_ALT,
-            CIU_CARVING_MENU
+            CIU_CATEGORY
     );
 }
